fcc8aad57a6154c6e6481fa66785c3662088de66
Charles Henry Le Nué <charles-henry.lenue@onrewind.com>
2023-05-19 10:01:11 +0200

    feat/ add solar systems

--- src/pages/Galaxy.jsx
+++ src/pages/Galaxy.jsx
@@ -7,6 +7,7 @@ import PageContent from '../common/PageContent';
 import '../common/css/galaxy.css';
 import SolarSystem from '../common/SolarSystem';
 import useSelectedElement from '../utils/customHooks/useSelectedElement';
+import SolarSystemItem from '../common/SolarSystemItem';
 
 const planets = [
   {
@@ -35,6 +36,27 @@ const planets = [
 function Galaxy() {
   const [elementSelected, setElementSelected] = useSelectedElement();
 
+  const planets1 = [
+    {
+      size: 25,
+      orbit: 250,
+      speed: 10,
+      color: '#9f4e17',
+    },
+    {
+      size: 55,
+      orbit: 380,
+      speed: 5,
+      color: 'grey',
+    },
+    {
+      size: 40,
+      orbit: 600,
+      speed: 40,
+      color: '#4c4c77',
+    },
+  ];
+
   return (
     <PageContainer>
       <PageHeader
@@ -46,9 +68,7 @@ function Galaxy() {
         setElementSelected={setElementSelected}
       />
 
-      <PageContent>
-        {/* <SolarSystem planets={planets} select={setElementSelected} /> */}
-      </PageContent>
+      <SolarSystemItem planets={planets1} sunColor={'#fae20a'} sunShadowColor={'orange'} size={800} sunSize={200} />
     </PageContainer>
   );
 }
