a6e53a044d9636985de1edc3f83fd8a3b3defd73
Charles Henry Le Nué <charleshenry.lenue@gmail.com>
2023-06-05 17:26:09 +0200

    feat/ add long press button with variants

--- src/common/MissionProgress.jsx
+++ src/common/MissionProgress.jsx
@@ -9,42 +9,73 @@ import CustomIcon from './CustomIcon';
 import RessourcesStack from './RessourcesStack';
 import PageContent from './PageContent';
 
-export default function MissionProgress({ progress, status, margin }) {
+export default function MissionProgress({ progress, status, margin, style, type }) {
   return (
-    <PageContent style={{ margin }}>
+    <PageContent style={{ margin, ...style }} borderLess={type === 'launch'}>
       <Stack direction='row' spacing={1} alignItems='center'>
-        <CustomIcon size={40} icon={'baseMission'} />
+        <CustomIcon
+          size={40}
+          icon={'baseMission'}
+          style={{ transform: 'rotate(90deg)', opacity: type === 'launch' && progress === 0 && 0.3 }}
+        />
 
-        {progress <= 50 ? (
+        {type === 'launch' ? (
           <>
-            <div style={{ width: `${progress * 2}%` }}>
+            <div style={{ width: `${progress}%`, opacity: 0 }}>
               <LinearProgress variant='determinate' value={100} />
             </div>
-            <CustomIcon size={30} icon={'spaceship'} style={{ transform: 'rotate(-90deg)' }} />
-            <div style={{ width: `${100 - progress * 2}%` }}>
-              <LinearProgress style={{ transform: 'rotate(180deg)' }} variant='buffer' value={0} valueBuffer={0} />
-            </div>
-          </>
-        ) : (
-          <>
-            <div style={{ width: `${100 - (progress - 50) * 2}%` }}>
+            <CustomIcon
+              size={30}
+              icon={'spaceship'}
+              style={{ transform: 'rotate(-90deg)', opacity: progress === 0 ? 0.3 : progress >= 100 ? 0 : 1 }}
+            />
+            <div style={{ width: `${100 - progress}%` }}>
               <LinearProgress
-                color={progress >= 100 && status === 'finish' ? 'success' : 'primary'}
-                variant='buffer'
+                style={{ transform: 'rotate(180deg)', opacity: progress > 0 ? 1 : 0 }}
+                variant={'buffer'}
                 value={0}
                 valueBuffer={0}
               />
             </div>
+          </>
+        ) : (
+          <>
+            {progress <= 50 ? (
+              <>
+                <div style={{ width: `${progress * 2}%` }}>
+                  <LinearProgress variant='determinate' value={100} />
+                </div>
+                <CustomIcon size={30} icon={'spaceship'} style={{ transform: 'rotate(-90deg)' }} />
+                <div style={{ width: `${100 - progress * 2}%` }}>
+                  <LinearProgress style={{ transform: 'rotate(180deg)' }} variant='buffer' value={0} valueBuffer={0} />
+                </div>
+              </>
+            ) : (
+              <>
+                <div style={{ width: `${100 - (progress - 50) * 2}%` }}>
+                  <LinearProgress
+                    color={progress >= 100 && status === 'finish' ? 'success' : 'primary'}
+                    variant='buffer'
+                    value={0}
+                    valueBuffer={0}
+                  />
+                </div>
 
-            <CustomIcon size={30} icon={'spaceship'} style={{ transform: 'rotate(90deg)' }} />
+                <CustomIcon size={30} icon={'spaceship'} style={{ transform: 'rotate(90deg)' }} />
 
-            <div style={{ width: `${(progress - 50) * 2}%` }}>
-              <LinearProgress color={progress >= 100 && status === 'finish' ? 'success' : 'primary'} variant='determinate' value={100} />
-            </div>
+                <div style={{ width: `${(progress - 50) * 2}%` }}>
+                  <LinearProgress
+                    color={progress >= 100 && status === 'finish' ? 'success' : 'primary'}
+                    variant='determinate'
+                    value={100}
+                  />
+                </div>
+              </>
+            )}
           </>
         )}
 
-        <CustomIcon size={40} icon={'arrival'} style={{ marginLeft: 'auto' }} />
+        {type !== 'launch' && <CustomIcon size={40} icon={'arrival'} style={{ marginLeft: 'auto' }} />}
       </Stack>
     </PageContent>
   );
