d6dca05788b35e3b1916a9ea0d4e8054450908d3
Charles Henry Le Nué <charleshenry.lenue@gmail.com>
2023-06-05 17:26:11 +0200

    feat/ use prev time to calcul ressources consumption

--- enums/game.js
+++ enums/game.js
@@ -1,7 +1,9 @@
 const startMoney = 100000;
 const productionDivider = 1000 * 60 * 60; // ms per hour
+const consumptionDivider = 25000 // to define
 
 module.exports = {
   startMoney,
-  productionDivider
+  productionDivider,
+  consumptionDivider
 };
--- server/production/checkRessources/checkConsumed.js
+++ server/production/checkRessources/checkConsumed.js
@@ -2,11 +2,11 @@ const logger = require('../../logger');
 const { decrementRessource } = require('../../helper/model.helper');
 const { getConsumedRessource } = require('../../helper/building.helper');
 
-async function checkConsumed(user, checkDate) {
+async function checkConsumed(user, prevTime, checkTime) {
   logger.info('   Check Consumed');
   try {
     const workingbuildings = user.Buildings.filter((building) => building.level > 0);
-    const workingPeople = user.Colonists;
+    const colonistsNbr = user.Colonists.length;
     const foodRessource = user.Ressources.find((ressource) => ressource.name === 'food');
 
     if (workingbuildings.length > 0) {
@@ -15,10 +15,9 @@ async function checkConsumed(user, checkDate) {
       await decrementRessource(consumed, energyRessource.id);
     }
 
-    if (workingPeople.length > 0) {
-      // const consumed = workingPeople.length;
-      // const consumed = getConsumedRessource(building.startTime, checkDate, building.output);
-      // await decrementRessource(consumed, foodRessource.id);
+    if (colonistsNbr > 0) {
+      const consumed = getConsumedRessource(prevTime, checkTime, colonistsNbr);
+      await decrementRessource(consumed, foodRessource.id);
     }
   } catch (error) {
     logger.error('checkConsumed', error);
